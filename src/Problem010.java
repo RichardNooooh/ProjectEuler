fcb1d28029391da9b3ef5e1fbe116b9e23f39cd8
Ice <richard.jun.noh@gmail.com>
2020-02-15 23:00:31 -0600

    Add fixed sieve and sum for Problem 010
    Although most of the algorithm appears to be correct, it still fails
    HackerRank's Test cases 4, 5, and 7.

--- src/Problem010.java
+++ src/Problem010.java
@@ -4,64 +4,59 @@ public class Problem010
 {
 	public static void main(String[] args)
 	{
-//		Scanner in = new Scanner(System.in);
-//		int t = in.nextInt();
-//		int[] testCases = new int[t];
-//
-//		int largestN = -1;
-//		for (int i = 0; i < t; i++)
-//		{
-//			int n = in.nextInt();
-//			if (n > largestN)
-//				largestN = n;
-//
-//			testCases[i] = n;
-//		}
-//
-//		m010(testCases, largestN);
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		int[] testCases = new int[t];
 
-		ArrayList<Integer> list = sieveOfEratosthenes(2000000);
-		long sum = 0;
-		for (int i : list)
-			sum += i;
-		System.out.println(sum + " sum: " + list.size());
+		int largestN = -1;
+		for (int i = 0; i < t; i++)
+		{
+			int n = in.nextInt();
+			if (n > largestN)
+				largestN = n;
+
+			testCases[i] = n;
+		}
+
+		m010(testCases, largestN);
 	}
 
-//	private static void m010(int[] testCases, int largestN)
-//	{
-//		ArrayList<Integer> primes = new ArrayList<Integer>();
-//		sieveOfEratosthenes(primes, largestN);
-//		System.out.println(primes);
-//
-////		ArrayList<Long> sumOfPrimes = new ArrayList<Long>();
-////		sumOfPrimes.add(2L);
-//
-//
-//
-//	}
+	private static void m010(int[] testCases, int largestN)
+	{
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		ArrayList<Long> sumOfPrimes = new ArrayList<Long>();
 
-//	private static int sumOfPrimes(ArrayList<Long> sums, ArrayList<Integer> addends)
-//	{
-//		if (addends.size() > sums.size())
-//		{
-//			int index = sums.size() - 1;
-//			int limit = addends.size() - 1;
-//
-//			while (index < limit)
-//			{
-//				sums.add(sums.get(index) + addends.get(index));
-//
-//				index++;
-//			}
-//
-//		}
-//
-//
-//
-//		return -1;
-//	}
+		sieveOfEratosthenes(primes, sumOfPrimes, largestN);
 
-	private static ArrayList<Integer> sieveOfEratosthenes(int n)
+		for (int n : testCases)
+		{
+			if (n == 1)
+				System.out.println("0");
+			else
+			{
+				int size = sumOfPrimes.size();
+				int i = 0;
+				boolean foundSum = true;
+				while (i < size && foundSum)
+				{
+					long curPrime = primes.get(i);
+					if (curPrime == n || i == size - 1)
+					{
+						System.out.println(sumOfPrimes.get(i));
+						foundSum = false;
+					}
+					else if (curPrime > n)
+					{
+						System.out.println(sumOfPrimes.get(i - 1));
+						foundSum = false;
+					}
+					i++;
+				}
+			}
+		}
+	}
+
+	private static void sieveOfEratosthenes(ArrayList<Integer> list, ArrayList<Long> sum, int n)
 	{
 		boolean prime[] = new boolean[n+1];
 
@@ -74,12 +69,16 @@ public class Problem010
 			}
 		}
 
-		ArrayList<Integer> out = new ArrayList<Integer>();
-
+		long prevSum = 0;
 		for(int i = 2; i <= n; i++)
+		{
 			if(prime[i] == false)
-				out.add(i);
-
-		return out;
+			{
+				list.add(i);
+				long thisSum = i + prevSum;
+				sum.add(thisSum);
+				prevSum = thisSum;
+			}
+		}
 	}
 }
