238f3453c54a6caac48785ed9d1f6639443e4db7
Ice <richard.jun.noh@gmail.com>
2020-02-16 23:55:38 -0600

    Add blank Problem007.java

---
+++ src/Problem007.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Problem007
+{
+
+	public static void main(String[] args)
+	{
+		ArrayList<Integer> primes = new ArrayList<Integer>();
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for (int a0 = 0; a0 < t; a0++)
+		{
+			int n = in.nextInt();
+
+		}
+	}
+
+
+}
